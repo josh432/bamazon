17cf15e6e1ae0c3d0c5c8b47decb8d36e3567e18
Josh Silverman <jrsilverman71@hotmail.com>
2018-01-24 13:24:13 -0800

    customer production selection & sale total done

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -11,14 +11,14 @@ var connection = mysql.createConnection({
 	database: 'bamazon'
 });
 
-var productsPurchased = [];
+var productPurchased = [];
 
 connection.connect();
 
 connection.query('SELECT item_id, product_name, price FROM products', function(err, result){
 	if (err) throw err;
 	console.log("connected as id: ", connection.threadID);
-	
+
 	var table = new Table ({
 		head: ['Item ID#', 'Product Name', 'Price'],
 		style: {
@@ -37,5 +37,45 @@ connection.query('SELECT item_id, product_name, price FROM products', function(e
 }
 console.log(table.toString());
 
-
+purchase();
 });
+
+var purchase = function() {
+	var productInfo = {
+		properties: {
+			item_id:{description: colors.blue('Please enter the ID# of the items you wish to buy.')},
+			Quantity:{description: colors.green('How many items would you like to buy?')}
+		},
+	};
+
+prompt.start();
+prompt.get(productInfo, function(err, res){
+	var custPurchase = {
+		item_id: res.item_id,
+		Quantity:res.Quantity
+	};
+
+	productPurchased.push(custPurchase);
+	connection.query('SELECT * FROM products WHERE item_id=?', productPurchased[0].item_id, function(err,res){
+		if (err) console.log(err, "That item ID doesn't exist");
+			
+		if (res[0].stock_quantity < productPurchased[0].Quantity){
+			console.log("That product is out of stock.");
+			connection.end();
+
+		} else if (res[0].stock_quantity >= productPurchased[0].Quantity){
+			console.log('');
+			console.log(productPurchased[0].Quantity + " items purchased");
+			console.log(productPurchased[0].product_name + ' ' + res[0].price);
+			var saleTotal = res[0].price * productPurchased[0].Quantity;
+			console.log('Total: ' + saleTotal);
+	
+
+		}
+
+
+	})
+
+})
+
+}
